054a8342153d18bf81d55d2cf9fe5eb814214e2c
Victor <victor.meyo@student.moringaschool.com>
2025-04-28 10:45:03 +0300

    Final Submission

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        # Turn the original word into a sorted list of letters
+        sorted_word = sorted(self.word.lower())
+
+        matches = []
+        for candidate in word_list:
+            if sorted(candidate.lower()) == sorted_word:
+                matches.append(candidate)
+
+        return matches
